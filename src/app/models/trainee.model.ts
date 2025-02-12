b8890bcca572b0c5dc1d2b57e9038c335d26f081
Emad Silawi <emad.silawi89@gmail.com>
2025-02-12 23:03:59 +0200

    Defining Models

--- src/app/models/trainee.model.ts
+++ src/app/models/trainee.model.ts
@@ -3,6 +3,12 @@ import { ITestResult } from "./testResult.model";
 export interface ITrainee {
     id: number;
     name: string;
+    email: string;
+    address: string;
+    city: string;
+    country: string;
+    zip: number;
     tests: ITestResult[];
+    average: number;
 }
   
